e5dc5de7cde805493adaef9474fa42c9b9baebf6
abhi <abhi@Life>
2022-10-15 14:11:04 +0530

    second commit

---
+++ addition/src/addition/addition1.java
@@ -0,0 +1,20 @@
+package addition;
+
+public class addition1 {
+	public static void main(String[] args) {
+		
+		int a = 20;
+		int b =30;
+		int c = a+b;
+		System.out.println(c);
+		
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.println(a<b);
+		System.out.println(a>b);
+		System.out.println(a>=b);
+	}
+
+}
